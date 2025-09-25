47c6c21d975bf18d5ae103fad50fb8e38f76e50a
Dzhamilya Iskhakova <jemmswsw@gmail.com>
2025-09-25 21:27:27 +0300

    Выполнение математических операций с введенными в консоль числами.

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("“Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число:");
+        int firstNumber;
+        firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int secondNumber;
+        secondNumber = new Scanner(System.in).nextInt();
+        System.out.println("Сумма:");
+        int sum=firstNumber+secondNumber;
+        System.out.println(sum);
+        System.out.println("Разность:");
+        int difference=firstNumber-secondNumber;
+        System.out.println(difference);
+        System.out.println("Произведение:");
+        int product=firstNumber*secondNumber;
+        System.out.println(product);
+        System.out.println("Частное:");
+        double quotient=(double)firstNumber/secondNumber;
+        System.out.println(quotient);
     }
 }
